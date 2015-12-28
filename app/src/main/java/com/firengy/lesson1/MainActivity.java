f98160d5f58b9c77198e6707ae24bb849a1c83fa
firengY <18811372352@163.com>
2015-12-28 17:48:20 +0800

    My First Commit

--- app/src/main/java/com/firengy/lesson1/MainActivity.java
+++ app/src/main/java/com/firengy/lesson1/MainActivity.java
@@ -1,10 +1,13 @@
 package com.firengy.lesson1;
 
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -12,6 +15,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private NavigationView menu;
     private DrawerLayout drawer;
     private ActionBarDrawerToggle toggle;
+    private TabLayout tabLayout;
+    private ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +25,38 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         menu = (NavigationView) findViewById(R.id.menu);
         drawer = (DrawerLayout) findViewById(R.id.drawer);
+        tabLayout = (TabLayout) findViewById(R.id.table_content);
+        pager = (ViewPager) findViewById(R.id.pager);
+
+
 
         toggle = new ActionBarDrawerToggle(this, drawer, 0, 0);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         drawer.setDrawerListener(toggle);
+
         menu.setNavigationItemSelectedListener(this);
         toggle.syncState();
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
 
-        return false;
+        int itemId = item.getItemId();
+        switch (itemId) {
+            case R.id.item_happen:
+                break;
+            case R.id.item_group:
+                break;
+        }
+        drawer.openDrawer(GravityCompat.START);
+        return true;
     }
 }
---
+++ app/src/main/java/com/firengy/lesson1/adapter/QiushiAdapter.java
@@ -0,0 +1,26 @@
+package com.firengy.lesson1.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by firengy
+ * on 15-12-28.
+ * Email: 18811372352@163.com
+ */
+public class QiushiAdapter extends FragmentPagerAdapter {
+    public QiushiAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
